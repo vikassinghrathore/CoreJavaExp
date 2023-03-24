742b7d405ef222dfc62fc064f53ccdf1e868fe3e
vikassinghrathore <vikaskanvika@gmail.com>
2023-03-24 15:45:11 +0530

    static method exp

---
+++ src/staticmethod/Java8Example.java
@@ -0,0 +1,17 @@
+package staticmethod;
+
+public class Java8Example implements StaticInterfaceExample {
+
+    @Override
+    public void existingabstractMethod(String str) {
+        System.out.println("Existing Java abstract method implementation");
+    }
+
+    public static void main(String[] args) {
+        Java8Example exp = new Java8Example();
+        exp.method();//call default method of interface
+        exp.existingabstractMethod("Java8 Features");
+        StaticInterfaceExample.staticMethod();//call static method using interface
+
+    }
+}
---
+++ src/staticmethod/StaticInterfaceExample.java
@@ -0,0 +1,15 @@
+package staticmethod;
+
+interface StaticInterfaceExample {
+    default void method() {
+        System.out.println("Java8 Default Method");
+    }
+
+    static void staticMethod() {
+        System.out.println("Java8 Static Method");
+    }
+
+    // abstract method, need to implement in implementing classes
+    public void existingabstractMethod(String str);
+}
+
